969ba529bd94985ec0e40fb7c0b7b8a195d4a87f
Matt <mburtonshaw@gmail.com>
2025-08-03 21:41:33 -0400

    still working new heroku url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,3 @@
 export default {
-    // apiBaseUrl: 'http://localhost:9000/',
-    apiBaseUrl: 'https://sloppy-jos-v2-3b638fb325b3.herokuapp.com/ '
+    apiBaseUrl: 'https://sloppy-jos-v2-3b638fb325b3.herokuapp.com/'
   };
